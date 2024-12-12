826854b1e5c8b4296b4ca295d4d8468bce6645cb
VPobaranchuk <vpobaranchuk@gmail.com>
2024-12-12 21:17:58 +0100

    Layout refactored: scroll fixed

--- src/layout/sections/shopCards/ShopCardsStyled.tsx
+++ src/layout/sections/shopCards/ShopCardsStyled.tsx
@@ -6,7 +6,7 @@ export const ShopCardsStyled = styled.div`
     align-items: center;
     justify-content: center;
     flex-wrap: wrap;
-    overflow-y: auto;
-    height: 85vh;
+    //overflow-y: auto; \
+    //height: 85vh;
     margin: 10px;
 `
